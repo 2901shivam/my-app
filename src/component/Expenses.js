6b16a2eaa154abf73f7657f1b96781702b3dcd37
= <shivamsingh201617@gmail.com>
2023-05-28 13:28:25 +0530

    change

--- src/component/Expenses.js
+++ src/component/Expenses.js
@@ -14,44 +14,36 @@ function Expenses(props) {
   const filteredexpense=props.items.filter(expense=>{
     return expense.date.getFullYear().toString()===filteredYear;
   })
+
+  let exxpenseContent=<p>No expense found</p>
+
+  if(filteredexpense.length>0){
+    exxpenseContent=filteredexpense.map((expense) => {
+      return (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      );
+    })}
+
+  
+
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandeler}
       />
-      {filteredexpense.map((expense) => {
-        return (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        );
-      })}
-      {/* <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      /> */}
+      {exxpenseContent}
+     
+    
     </Card>
   );
 }
+
 
 export default Expenses;
